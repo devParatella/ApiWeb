df87a5405e545172a367dfb55bba567ff9a1cee5
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-14 20:20:19 -0300

    Consumo de API Ramdom Fox

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,35 @@
-const BASE_URL = "https://api.thecatapi.com/v1/images/search";
+const BASE_URL = "https://randomfox.ca/floof";
 
 const trocaimg = async () => {
   try {
-    const data = await fetch(BASE_URL);
-    const json = await data.json();
+    const response = await fetch(BASE_URL);
 
-    return json[0].url;
-  } catch (erro) {
-    console.log("ocorreu um erro ao carregar a imagem");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data.image;
+  } catch (error) {
+    console.error("ocorreu um erro ao carregar a imagem", error);
+    throw error;
   }
 };
 
 const loadImg = async () => {
   const img = document.getElementsByTagName("img")[0];
-  img.src = await trocaimg();
+  try {
+    const imageUrl = await trocaimg();
+    img.src = imageUrl;
+  } catch (erro) {
+    console.error("ocorreu um erro ao carregar", erro);
+  }
 };
 
 loadImg();
 
 const btn = document.getElementById("trocaimg");
-btn.addEventListener("click", loadImg);
+if (btn) {
+  btn.addEventListener("click", loadImg);
+} else {
+  console.warm("Elemento de botão com ID 'trocaimg' não encontrado ");
+}
