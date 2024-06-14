13f1ddb714e6106c5c998ba39cbc6156d0cef0f6
paratellaDev <paratelladevmarcos@gmail.com>
2024-06-14 10:06:57 -0300

    removendo true

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,22 @@
-const BASE_URL = 'https://api.thecatapi.com/v1/images/search'
+const BASE_URL = "https://api.thecatapi.com/v1/images/search";
 
-const trocaimg = async () =>{
-    try{
-        const data = await fetch(BASE_URL)
-        const json = await data.json()
-      
+const trocaimg = async () => {
+  try {
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
 
-     return json[0].url;
-        
-        
-    }catch(erro){
-        console.log("ocorreu um erro ao carregar a imagem");
-    
-}
+    return json[0].url;
+  } catch (erro) {
+    console.log("ocorreu um erro ao carregar a imagem");
+  }
 };
 
-const loadImg = async () =>{
-    const img = document.getElementsByTagName('img')[0];
-    img.src = await trocaimg();
-}
-
-
+const loadImg = async () => {
+  const img = document.getElementsByTagName("img")[0];
+  img.src = await trocaimg();
+};
 
 loadImg();
 
-const btn = document.getElementById('trocaimg');
-btn.addEventListener('click',loadImg,true);
+const btn = document.getElementById("trocaimg");
+btn.addEventListener("click", loadImg);
